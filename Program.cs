db110b5aaab556b153a81585383bcc3bd170ff11
joehall06 <152999291+joehall06@users.noreply.github.com>
2024-03-21 11:36:36 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿static void linear_search()
+{
+    string[] items = { "orange", "banana", "apple", "strawberries", "rasberries", "pear", "grapefruit" };
+    Console.WriteLine("Enter the fruit to find: ");
+    string? itemToFind = Console.ReadLine();
+
+    int index = 0;
+    bool found = false;
+
+    while (index < items.Length)
+    {
+        if (items[index] == itemToFind )
+        {
+            Console.WriteLine("Value found at index " + index);
+            found = true;
+            break;
+        }
+        index++;
+    }
+
+    if (found == false)
+    {
+        Console.WriteLine("Value not found");
+    }
+
+}
+
+linear_search();
